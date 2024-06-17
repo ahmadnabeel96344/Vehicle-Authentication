bac7a7f12bae3929a41a65410f6fad7abb1b4112
fuzailAhmad123 <fuzailahmad2605@gmail.com>
2024-06-17 22:28:38 +0530

    frontend uri added

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173", // Allow only the frontend origin
+    origin: "https://vehicle-authentication.vercel.app/", // Allow only the frontend origin
     credentials: true, // Allow credentials if needed
 }));
 
